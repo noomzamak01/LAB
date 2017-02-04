d57a3df40be2287eb165bffb436caddc9e8d5815
Khan Pongsupat <noomzamak01@gmail.com>
2017-02-04 09:29:00 +0700

    LAB

---
+++ demo2017/Assets/Script/SceneBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneBall : MonoBehaviour {
+
+	public Text txtScore;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	int score = 0;
+
+	public void DoPushButton(){
+		score++;
+
+
+		txtScore.text = "Score : " + score.ToString ();
+
+	}
+	public void DoChangScene(){
+		SceneManager.LoadScene (1);
+	}
+	public void nstartscnce (){
+		SceneManager.LoadScene (0);
+	}
+
+}
